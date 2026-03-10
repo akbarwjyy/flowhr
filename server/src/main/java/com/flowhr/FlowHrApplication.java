dd11eeb49e8e417778e26639eaeab71c80d4d635
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2026-03-10 19:55:12 +0700

    feat: Add server-side analytics controller and install client dependencies.

---
+++ server/src/main/java/com/flowhr/FlowHrApplication.java
@@ -0,0 +1,11 @@
+package com.flowhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowHrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowHrApplication.class, args);
+    }
+}
